786f0abd2cd17da386b6480360e16c03acbb5077
Luma <world@luma.email>
2025-05-05 15:22:49 +0900

    minor: Feature to parse and print as tex math transforms in quick notation (#6)
    * minor: Feature to parse and print as tex math transforms in quick notation
    
    * lint-fix

---
+++ src/katex-ex/__tests__/transform.spec.ts
@@ -0,0 +1,127 @@
+import {
+  parseMathTransform,
+  makeMathTransform1Column,
+  makeMathTransform2Column,
+} from "../transform.js";
+import { describe, it, expect } from "vitest";
+
+describe("parseMathTransform", () => {
+  it("basic", () => {
+    expect(
+      parseMathTransform(
+        ["x", "= x^2", "~ x^3", "$ desc1", "- x^4", "? desc2"].join("\n"),
+      ),
+    ).toMatchInlineSnapshot(`
+      {
+        "first": "x",
+        "steps": [
+          {
+            "formula": "x^2",
+            "prefix": "= ",
+          },
+          {
+            "description": "desc1",
+            "formula": "x^3",
+            "prefix": "\\sim ",
+          },
+          {
+            "descIsText": true,
+            "description": "desc2",
+            "formula": "x^4",
+            "prefix": "\\iff ",
+          },
+        ],
+      }
+    `);
+  });
+});
+
+describe("makeMathTransform1Column", () => {
+  it("basic", () => {
+    expect(
+      makeMathTransform1Column({
+        first: "x",
+        steps: [
+          {
+            formula: "x^2",
+            prefix: "=",
+          },
+          {
+            formula: "x^3",
+            prefix: "\\sim ",
+          },
+          {
+            formula: "x^4",
+            prefix: "\\iff ",
+          },
+        ],
+      }),
+    ).toMatchInlineSnapshot(`
+      "\\begin{aligned}
+      &&&x&&\\\\
+      &=&&x^2&&\\\\
+      &\\sim &&x^3&&\\\\
+      &\\iff &&x^4&&\\\\
+      \\end{aligned}"
+    `);
+  });
+});
+
+describe("makeMathTransform2Column", () => {
+  it("basic", () => {
+    expect(
+      makeMathTransform2Column({
+        first: "x",
+        steps: [
+          {
+            formula: "x^2",
+            prefix: "=",
+          },
+          {
+            formula: "x^3",
+            prefix: "\\sim ",
+          },
+          {
+            formula: "x^4",
+            prefix: "\\iff ",
+          },
+        ],
+      }),
+    ).toMatchInlineSnapshot(`
+      "\\begin{aligned}
+      &x&&=&&x^2&&\\\\
+      &&&\\sim &&x^3&&\\\\
+      &&&\\iff &&x^4&&\\\\
+      \\end{aligned}"
+    `);
+  });
+  it("with description", () => {
+    expect(
+      makeMathTransform2Column({
+        first: "x",
+        steps: [
+          {
+            formula: "x^2",
+            prefix: "=",
+            description: "desc1",
+          },
+          {
+            formula: "x^3",
+            prefix: "\\sim ",
+          },
+          {
+            formula: "x^4",
+            prefix: "\\iff ",
+            description: "desc2",
+          },
+        ],
+      }),
+    ).toMatchInlineSnapshot(`
+      "\\begin{aligned}
+      &x&&=&&x^2&&\\left({desc1}\\right)\\\\
+      &&&\\sim &&x^3&&\\\\
+      &&&\\iff &&x^4&&\\left({desc2}\\right)\\\\
+      \\end{aligned}"
+    `);
+  });
+});
