8f80c38aff7586a8718074303c20fa1a7aed103a
Luma <world@luma.email>
2025-06-08 03:37:39 +0900

    feat(rehype-proc-term): termについてそれを直接囲む要素を表すtermContainerを追加(一部のタグのみ)

--- src/rehype-proc-term.ts
+++ src/rehype-proc-term.ts
@@ -31,6 +31,25 @@ export type RehypeProcTermPluginParams = {
   readonly termProcessor: TermProcessorProtocol;
 };
 
+export type TermContainer = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+export const parseConatiner = (parent: unknown): TermContainer | null => {
+  if (typeof parent !== "object" || parent === null) return null;
+  if (!("type" in parent) || typeof parent.type !== "string") return null;
+  if (!("tagName" in parent) || typeof parent.tagName !== "string") return null;
+  if (parent.type !== "element") return null;
+
+  switch (parent.tagName) {
+    case "h1":
+    case "h2":
+    case "h3":
+    case "h4":
+    case "h5":
+    case "h6":
+      return parent.tagName;
+  }
+  return null;
+};
+
 export type RehypeProcTermPlugin = import("unified").Plugin<
   [RehypeProcTermPluginParams],
   Root
@@ -38,7 +57,7 @@ export type RehypeProcTermPlugin = import("unified").Plugin<
 const rehypeProcTerm: RehypeProcTermPlugin = ({ termProcessor }) => {
   return async (tree) => {
     const refCount = new Map<string, number>();
-    await visitAsync(tree, async (node) => {
+    await visitAsync(tree, async (node, parents) => {
       if (node.type !== "text" || typeof node.value !== "string") return;
 
       const newChildren: MdxJsxTextElement["children"] = [];
@@ -59,6 +78,7 @@ const rehypeProcTerm: RehypeProcTermPlugin = ({ termProcessor }) => {
             break;
           case "term": {
             const refIndex = refCount.get(parsed.term) ?? 0;
+            const termContainer = parseConatiner(parents.at(-1)?.node);
             refCount.set(parsed.term, refIndex + 1);
             newChildren.push({
               type: "mdxJsxTextElement",
@@ -78,6 +98,17 @@ const rehypeProcTerm: RehypeProcTermPlugin = ({ termProcessor }) => {
                     value: "",
                     data: {
                       estree: estreeJsonParseOf(refIndex),
+                    },
+                  },
+                },
+                {
+                  type: "mdxJsxAttribute",
+                  name: "termContainer",
+                  value: {
+                    type: "mdxJsxAttributeValueExpression",
+                    value: "",
+                    data: {
+                      estree: estreeJsonParseOf(termContainer),
                     },
                   },
                 },
